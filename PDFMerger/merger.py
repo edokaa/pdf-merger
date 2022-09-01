c8b4e370deed50e9d77f872732fce402493d555d
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-09-01 17:50:02 +0100

    error handling: no file selected

--- PDFMerger/merger.py
+++ PDFMerger/merger.py
@@ -7,32 +7,43 @@ class PDFMerger:
     def openFiles(self):
         print('>>> Select the pdf files to merge')
         files = askopenfilenames()
-        if len(files) < 2:
+        if len(files) == 1:
             print('>>> Kindly Select more than one pdf file')
             return self.openFiles()
+        elif len(files) == 0:
+            files = None
         else:
-            return files
+            for item in files:
+                if item[-4:] != '.pdf':
+                    print('Only .pdf files are supported!!! ')
+                    return self.openFiles()
+        return files
 
     def mergeFiles(self):
         pdfFiles = self.openFiles()
-        pdfWriter = PyPDF2.PdfFileWriter()
+        if pdfFiles is not None:
+            pdfWriter = PyPDF2.PdfFileWriter()
 
-        for filename in pdfFiles:
-            pdfFileObj = open(filename, 'rb')
-            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+            for filename in pdfFiles:
+                pdfFileObj = open(filename, 'rb')
+                pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-            for pageNum in range(pdfReader.numPages):
-                pageObj = pdfReader.getPage(pageNum)
-                pdfWriter.addPage(pageObj)
-        print('>>> Merging Done!!!')
-        return pdfWriter
+                for pageNum in range(pdfReader.numPages):
+                    pageObj = pdfReader.getPage(pageNum)
+                    pdfWriter.addPage(pageObj)
+            print('>>> Merging Done!!!')
+            return pdfWriter
+        return None
 
     def save(self):
         pdfWriter = self.mergeFiles()
-        outputFileName = input('>>>Enter Output File name: ')
-        print('>>> Select Output Directory')
-        outputDir = askdirectory()
-        pdfOutput = open(f"{outputDir}\\{outputFileName}.pdf", 'wb')
-        pdfWriter.write(pdfOutput)
-        pdfOutput.close()
-        print('>>> Saved Successfully!!!')
+        if pdfWriter is not None:
+            outputFileName = input('>>>Enter Output File name: ')
+            print('>>> Select Output Directory')
+            outputDir = askdirectory()
+            print('output: ', outputDir)
+            pdfOutput = open(f"{outputDir}\\{outputFileName}.pdf", 'wb')
+            pdfWriter.write(pdfOutput)
+            pdfOutput.close()
+            print('>>> Saved Successfully!!!')
+        
